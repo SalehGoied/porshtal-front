de44c745c1f2823cc28da7ae814f0d0d6e9509eb
Saleh Goied <salehgoied@gmail.com>
2024-08-04 13:56:20 +0300

    test-3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
